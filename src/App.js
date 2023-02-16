1f6d2fccc6179db36e4c4ad4ccfe1161ccd4c3bd
Vijay <vijayktpr1@gmail.com>
2023-02-16 12:28:44 +0530

    Updated link href

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ function App() {
     <div className="App">
      <div className="w3-top">
   <div className="w3-bar w3-black w3-card w3-left-align w3-large">
-    <a className="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onClick="myFunction()" title="Toggle Navigation Menu"><i className="fa fa-bars"></i></a>
-    <a href="#" className="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
-    <a href="#" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 1</a>
-    <a href="#" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 2</a>
-    <a href="#" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 3</a>
-    <a href="#" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 4</a>
+    <a href="/test" className="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
+    <a href="/test" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 1</a>
+    <a href="/test" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 2</a>
+    <a href="/test" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 3</a>
+    <a href="/test" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 4</a>
   </div>
 
 
   <div id="navDemo" className="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-    <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 1</a>
-    <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 2</a>
-    <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 3</a>
-    <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 4</a>
+    <a href="/test" className="w3-bar-item w3-button w3-padding-large">Link 1</a>
+    <a href="/test" className="w3-bar-item w3-button w3-padding-large">Link 2</a>
+    <a href="/test" className="w3-bar-item w3-button w3-padding-large">Link 3</a>
+    <a href="/test" className="w3-bar-item w3-button w3-padding-large">Link 4</a>
   </div>
 </div>
 <div className="w3-row-padding w3-padding-64 w3-container">
